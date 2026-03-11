82ac57c1b7ae089f0b68768ed143c2eab06c8331
Cuban Gnocchi <cubangnocchi@proton.me>
2026-03-11 19:17:32 -0400

    checkpoint

--- src/inventory/inventory.py
+++ src/inventory/inventory.py
@@ -2,9 +2,10 @@ from src.inventory.human import Human
 from src.inventory.item import Item
 
 class Inventory:
-    def __init__(self, items: list[Item], people: list[Human]):
+    def __init__(self, items: dict[str, Item], people: dict[str, Human], next_key: int):
         self.items = items
         self.people = people
+        self.next_key = next_key #!each time an item or something is added, this goes ++ 
     
     def get_items(self):
         return self.items
@@ -12,6 +13,52 @@ class Inventory:
     def get_people(self):
         return self.people
     
+    def get_item(self, key: str):
+        return self.items[key]
+    
+    def get_person(self, key: str):
+        return self.people[key]
+    
+    def get_items_keys(self):
+        return list(self.items.keys())
+    
+    def get_people_keys(self):
+        return list(self.people.keys())
+    
+    def convert_to_dict(self):
+        item_keys = self.get_items_keys()
+        people_keys = self.get_people_keys()
+        converted_items = {}
+        converted_humans = {}
+
+        for key in item_keys:
+            converted_items[key] = self.items[key].convert_to_dictionary()
+
+        for key in people_keys:
+            converted_humans[key] = self.people[key].convert_to_dictionary()
+
+        output = {
+            "ITEMS": converted_items,
+            "PEOPLE": converted_humans 
+        }
+
+        return output
+    
+    @staticmethod
+    def convert_dict_to_inventory(inventory_dict: dict[str, dict]):
+        
+        items_as_dict = inventory_dict["ITEMS"]
+        humans_as_dict = inventory_dict["PEOPLE"]
+
+        item_keys = list(items_as_dict.keys())
+        people_keys = list(humans_as_dict.keys())
+
+        
+
+        for key in item_keys:
+
+        
+
 
     
     #! hay que ver cómo poder hacer referencia a los items por un index mmmmmmmmmmmmmmmmmmmmmmm
