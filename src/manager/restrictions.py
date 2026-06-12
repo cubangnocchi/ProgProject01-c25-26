92ac7c72e2295ca961a33dd44bad2aac785e56b7
Cuban Gnocchi <cubangnocchi@proton.me>
2026-06-12 19:27:04 -0400

    checkpoint

--- src/manager/restrictions.py
+++ src/manager/restrictions.py
@@ -1,40 +1,21 @@
 from src.calendar.event_class.interval import interval
 from src.calendar.event_class.event import Event
 from src.inventory.item import Item
+from src.calendar.calendar import Calendar
+from src.inventory.inventory import Inventory
 
-# -------------[Time restriction methods]---------------------------------------------------------------------------
-def global_time_consistency_check (actual_time, events_list):
-
-    if(not valid_from_actual_time(actual_time, events_list[0])):
-        return False
-    
-    for event_a in events_list:
-        if(time_consistency_check(event_a, events_list)):
-            return False
-            
-    return True
-
-def time_consistency_check (event, events_list):
-
-    for event_a in events_list:
-        if(interval.is_it_overlaping(event_a.get_interval(), event.get_interval())):
-            return False
-            
-    return True
-
-def valid_from_actual_time(actual_time, event):
-    
-    if(event.get_sarting_date() < actual_time):
-        return False
-        
-    return True
+#temporal global variables for this
 
 
-# ------------[Inventory consistency methods]
+def check_event(event: Event, calendar: Calendar, inventory: Inventory):
 
-# key coincidences
+    error_list = []
 
-# check the comment in inventory
+    return error_list
+
+
+
+
 
 
 
