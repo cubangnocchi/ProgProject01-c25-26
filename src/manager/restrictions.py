e0794b6fb918516eaed3877790006c74b0f9c51c
Cuban Gnocchi <cubangnocchi@proton.me>
2026-06-16 14:59:53 -0400

    checkpoint

--- src/manager/restrictions.py
+++ src/manager/restrictions.py
@@ -29,11 +29,29 @@ def exclusion_check(event: Event, inventory: Inventory):
     
     return error_list
 
+# called from check_event, checks:
+# - no person or unique item is in to places at the same time
+# - the amount of a certain item used along all the events doesn't 
+#   end with a negative mass paradox and sumons a warp drive...
 def resources_n_crew_concistency(event: Event, calendar: Calendar, inventory: Inventory):
     #output
     error_list = []
     
     return error_list
+
+def place_related_restrictions(event: Event, inventory: Inventory):
+    #output
+    error_list = []
+    
+    return error_list
+
+def dynamic_restrictions(event: Event, inventory: Inventory):
+    #output
+    error_list = []
+    
+    return error_list
+
+
 
 
 
